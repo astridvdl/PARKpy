1056797e7b97a42fccb7517639228f579842e4ad
preston.padayachee <Preston.Padayachee@zutari.com>
2022-04-05 23:29:48 +0200

    added min_width function

--- src/category.py
+++ src/category.py
@@ -12,19 +12,28 @@ class Category:
         self.df = pd.read_csv(self.path)
         return self.df
 
-    def catergorize(self, df):
+    def split_size(self, df):
         self.df = df
         self.df[['Size_1', 'Size_2', 'Size_3']] = self.df['Size'].str.split('-', expand=True)
         self.df[['Size_1_W', 'Size_1_H']] = self.df['Size_1'].str.split('x', expand=True)
         self.df[['Size_2_W', 'Size_2_H']] = self.df['Size_2'].str.split('x', expand=True)
         self.df[['Size_3_W', 'Size_3_H']] = self.df['Size_3'].str.split('x', expand=True)
+        self.df[['Size_1_W', 'Size_1_H']] = self.df[['Size_1_W', 'Size_1_H']].apply(pd.to_numeric)
+        self.df[['Size_2_W', 'Size_2_H']] = self.df[['Size_2_W', 'Size_2_H']].apply(pd.to_numeric)
+        self.df[['Size_3_W', 'Size_3_H']] = self.df[['Size_3_W', 'Size_3_H']].apply(pd.to_numeric)
         return self.df
 
+    def min_width(self, df):
+        self.df = df
+        self.df['Min_Width'] = self.df[['Size_1_W', 'Size_2_W', "Size_3_W"]].min(axis=1)
+        return self.df
+        
 
 if __name__ == "__main__":
     df1_obj = Category("../data/Duct Fitting Schedule Low Pressure Insulated Rect.csv")
     df1 = df1_obj.dataframe()
-    df1 = df1_obj.catergorize(df1)
+    df1 = df1_obj.split_size(df1)
+    df1 = df1_obj.min_width(df1)
     print(df1)
-    df1.to_csv("test_out.csv",index = False)
+    df1.to_csv("Preston2.csv",index = False)
     
