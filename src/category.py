a355239706cbfa76ced54590a0953a065a1af3d8
preston.padayachee <Preston.Padayachee@zutari.com>
2022-04-05 20:18:38 +0200

    added dataframe function to read csv

--- src/category.py
+++ src/category.py
@@ -1,2 +1,19 @@
+import os
+import pandas as pd
+
+
 class Category:
-    pass
+    
+    def __init__(self, path):
+        self.abs_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)))
+        self.path = os.path.join(self.abs_path, path)
+
+    def dataframe(self):
+        self.df = pd.read_csv(self.path)
+        return self.df
+
+if __name__ == "__main__":
+    df1_obj = Category("../data/Duct Fitting Schedule Low Pressure Insulated Rect.csv")
+    df1 = df1_obj.dataframe()
+    print(df1)
+    
