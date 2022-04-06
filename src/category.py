b52d84fa680835eec678ecb3908df6200e3a1ebc
preston.padayachee <Preston.Padayachee@zutari.com>
2022-04-06 08:02:07 +0200

    find max & sum (width, height)

--- src/category.py
+++ src/category.py
@@ -33,12 +33,19 @@ class Category:
         self.df['Min_Height'] = self.df[['Size_1_H', 'Size_2_H', "Size_3_H"]].min(axis=1)
         return self.df    
 
+    def categorize(self, df):
+        self.df = df
+        self.df['Max_W_H'] = self.df[['Min_Width','Min_Height']].max(axis=1)
+        self.df['Sum_W_H'] = self.df['Min_Width'] + self.df['Min_Height']
+        return self.df
+
 if __name__ == "__main__":
     df1_obj = Category("../data/Duct Fitting Schedule Low Pressure Insulated Rect.csv")
     df1 = df1_obj.dataframe()
     df1 = df1_obj.split_size(df1)
     df1 = df1_obj.min_width(df1)
     df1 = df1_obj.min_height(df1)
+    df1 = df1_obj.categorize(df1)
     print(df1)
     df1.to_csv("test_out.csv",index = False)
     
