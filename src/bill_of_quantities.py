a62ea916b7b946296ea5fa2b13d0b671e69c85f3
astridvdl <astrid.vanderlaan@zutari.com>
2022-04-07 08:25:38 +0200

    refactored to OOP

--- src/bill_of_quantities.py
+++ src/bill_of_quantities.py
@@ -1,11 +1,13 @@
 import os
 import pandas as pd
+import numpy as np
 
 class BillOfQuantities:
-    def __init__(self, data):
+    
+    def __init__(self, data_path):
         self.content_list = []
         self.file_names = []
-        self.clean_data(data)
+        self.clean_data(data_path)
 
     def clean_data(self, data):
         #for file in [f for f in os.listdir(data) if f.endswith(".csv")]:
@@ -14,17 +16,52 @@ class BillOfQuantities:
                 file_path = os.path.join(data,file)
                 df = pd.read_csv(file_path, index_col=0)
                 df = df[df["Size"].notnull()]
-                #df.to_csv(f"output/temp/cleaned_{file}",index = False)
+                
+                df['File'] = file
+
                 self.content_list.append(df)
                 self.file_names.append(file)
         
-        singular_df = pd.concat(self.content_list)
-        singular_df.to_csv("output/csv/all_cleaned.csv",index = False)
-        print(singular_df) 
+        self.content = pd.concat(self.content_list)
+        #self.combine_all()
+        
+        #self.content.to_csv("output/csv/all_cleaned.csv",index = False)
+        print(self.content) 
 
 
-    def export_to_csv(self):
-        return
+    def split_size(self):
+        self.content[['Size_1', 'Size_2', 'Size_3']] = self.content['Size'].str.split('-', expand=True)
+        self.content[['Size_1_W', 'Size_1_H']] = self.content['Size_1'].str.split('x', expand=True)
+        self.content[['Size_2_W', 'Size_2_H']] = self.content['Size_2'].str.split('x', expand=True)
+        self.content[['Size_3_W', 'Size_3_H']] = self.content['Size_3'].str.split('x', expand=True)
+        self.content[['Size_1_W', 'Size_1_H']] = self.content[['Size_1_W', 'Size_1_H']].apply(pd.to_numeric)
+        self.content[['Size_2_W', 'Size_2_H']] = self.content[['Size_2_W', 'Size_2_H']].apply(pd.to_numeric)
+        self.content[['Size_3_W', 'Size_3_H']] = self.content[['Size_3_W', 'Size_3_H']].apply(pd.to_numeric)
 
-    def export_to_excel(self):
-        return
+    def min_width(self):
+        self.content['Min_Width'] = self.content[['Size_1_W', 'Size_2_W', "Size_3_W"]].min(axis=1)
+
+
+    def min_height(self):
+        self.content['Min_Height'] = self.content[['Size_1_H', 'Size_2_H', "Size_3_H"]].min(axis=1)  
+
+    def categorize(self):
+        self.content['Max_W_H'] = self.content[['Min_Width','Min_Height']].max(axis=1)
+        self.content['Sum_W_H'] = self.content['Min_Width'] + self.content['Min_Height']
+        self.conditions = [
+            (self.content['Max_W_H'] < 750) & (self.content['Sum_W_H'] <= 1150),
+            (self.content['Max_W_H'] < 750) & (self.content['Sum_W_H'] > 1150),
+            (self.content['Max_W_H'] >= 750) & (self.content['Max_W_H'] < 1350),
+            (self.content['Max_W_H'] >= 1350) & (self.content['Max_W_H'] < 2100),
+            (self.content['Max_W_H'] >= 2100)
+        ]
+        self.category = [1, 2, 3, 4, 5]
+        self.content['Category'] = np.select(self.conditions, self.category)
+
+
+    def export(self,location):
+        for file in self.content.File.unique():  
+            print(file)      
+            file_data = self.content.loc[self.content["File"] == file]
+            file_data.to_csv(f"{location}\\BOQ_{file}")
+            
