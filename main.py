803e8c87c4b713d0a79b1fc967e2a8c7867d9d0a
astridvdl <Astrid.vanderLaan@aurecongroup.com>
2022-04-09 00:23:34 +0200

    refactored for OOP

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.bill_of_quantities import BillOfQuantities
 
 if __name__ == "__main__":
-    boq_categories = {
+    boq_hvac_categories = {
         1: 1.00,
         2: 2.50,
         3: 4.00,
@@ -9,9 +9,9 @@ if __name__ == "__main__":
         5: 6.00
         }
 
-    mechanical_boq = BillOfQuantities("raw_data")
+    my_boq = BillOfQuantities("raw_data")
     #mechanical_boq.create_boq_sections(boq_categories)
-    mechanical_boq.create_boq(boq_categories)
-
-    #mechanical_boq.export_CSV("output\\csv")
+    my_boq.create_boq("AIR CONDITIONING & VENTILATION INSTALLATION") #create excel
+    my_boq.create_boq_sections(boq_hvac_categories)
+    my_boq.export_seperate_csvs("output\\csv")
     #mechanical_boq.export_Excel("output\\excel)
