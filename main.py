0f0169eb86d8f5b6509c9d561e0aaa769bab3136
astridvdl <102604534+astridvdl@users.noreply.github.com>
2022-04-09 00:38:14 +0200

    Revert "update_refactored for OOP"

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from src.bill_of_quantities import BillOfQuantities
+import pandas as pd
 
 if __name__ == "__main__":
-    boq_hvac_categories = {
+    boq_categories = {
         1: 1.00,
         2: 2.50,
         3: 4.00,
@@ -9,9 +10,6 @@ if __name__ == "__main__":
         5: 6.00
         }
 
-    my_boq = BillOfQuantities("raw_data")
-    #mechanical_boq.create_boq_sections(boq_categories)
-    my_boq.create_boq("AIR CONDITIONING & VENTILATION INSTALLATION") #create excel
-    my_boq.create_boq_sections(boq_hvac_categories)
-    my_boq.export_seperate_csvs("output\\csv")
-    #mechanical_boq.export_Excel("output\\excel)
+    mechanical_boq = BillOfQuantities("raw_data",boq_categories)
+    mechanical_boq.create()
+    mechanical_boq.export("output\\csv")
